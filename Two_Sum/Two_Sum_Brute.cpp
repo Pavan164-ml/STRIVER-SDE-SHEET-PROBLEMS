cf71ce24517aa39c00929e4e8f6620d3107c2116
Pavan Kumar <59686265+Pavan164-ml@users.noreply.github.com>
2022-06-15 12:52:02 +0530

    Update Two_Sum_Brute.cpp

--- Two_Sum/Two_Sum_Brute.cpp
+++ Two_Sum/Two_Sum_Brute.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        for(int i = 0 ; i < nums.size()-1 ; i++)
-            for(int j = i+1 ; j < nums.size() ; j++)
+        for(int i = 0 ; i < nums.size()-1 ; i++)                // Since we donot want to add the same element here we start from 0 and go till n-1 so that last element doesnt get added twice
+            for(int j = i+1 ; j < nums.size() ; j++)            // And same we start here from the next element of i^th index
                 if(nums[i]+nums[j] == target)
                     return vector<int> {i,j};
                     return {};
