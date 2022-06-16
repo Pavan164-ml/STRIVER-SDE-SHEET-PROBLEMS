34061a7da92308f0ab8a89b5a0ab307b4386e377
Pavan Kumar <59686265+Pavan164-ml@users.noreply.github.com>
2022-06-16 19:27:19 +0530

    Update pascals.cpp

--- Pascals/pascals.cpp
+++ Pascals/pascals.cpp
@@ -5,10 +5,13 @@ public:
 
         for (int i = 0; i < numRows; i++) {
             r[i].resize(i + 1);
+            // Since the next line in the triangle should have more numbers that the previous one
             r[i][0] = r[i][i] = 1;
+            // Since the first and last element should be 1 
   
             for (int j = 1; j < i; j++)
                 r[i][j] = r[i - 1][j - 1] + r[i - 1][j];
+            // Here we change only the column but the present element is the sum of previous row'th elements in different colums
         }
         
         return r;
