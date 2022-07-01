21547cc046f4941193b43c62b423905b27835d93
Pavan Kumar <59686265+Pavan164-ml@users.noreply.github.com>
2022-07-01 15:45:22 +0530

    Update Set_Zeros_Better.cpp

--- Arrays Part 1/1. Set_Zeros/Set_Zeros_Better.cpp
+++ Arrays Part 1/1. Set_Zeros/Set_Zeros_Better.cpp
@@ -2,7 +2,10 @@
 
 void setZeroes(vector < vector < int >> & matrix) {
   int rows = matrix.size(), cols = matrix[0].size();
+//   Declaing the dummy vectors with single dimension
   vector < int > dummy1(rows,-1), dummy2(cols,-1);
+  
+//   Initializing the dummy variables with 0's all
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
       if (matrix[i][j] == 0) {
@@ -12,6 +15,8 @@ void setZeroes(vector < vector < int >> & matrix) {
     }
 
   }
+  
+//   We Just manipulate the main matrix when either the row or the column contains 0 value in it
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
       if (dummy1[i] == 0 || dummy2[j]==0) {
